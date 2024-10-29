b6e63c5df6e33104a18a503835597ad28db14060
DiegoDomingue-Z <runtime576@gmail.com>
2024-10-29 14:57:08 -0600

    rest server completo

---
+++ routes/usuarios.js
@@ -0,0 +1,51 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+const {
+usuariosConsulta,
+actualizarUsuario,
+usuarioId,
+eliminarUsuario,
+crearUsuario} = require('../controllers/usuarios');
+const validarCampos = require('../middlewares/validarCampos');
+const { rolValido, emailExistee, existeId } = require('../helpers/db-validaciones');
+
+const router = Router();
+
+router.get('/', usuariosConsulta);
+
+router.get('/:id', usuarioId);
+
+router.post('/',[
+    //al colocar not.isEmpty le estamos diciendo si el campo no esta bacio 
+    //manda el mensaje del campo es obligatorio
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('contrasena', 'La contrseña debe tener 6 caracteres minimo').isLength({min:6, max:10}),
+    //el campo correo lo estraemos del body de la request o de lo manda el cliente
+    check('correo').isEmail().withMessage('Debes colocar un correo valido'),
+    //al colocarle isin es como decirle si esta en mi array entonces agregalo 
+    //check('rol','No es un rol valido').isIn(['Admin','Agente','Tecnico']),
+
+    //con el custom podemoa hacer verficaciones personalizadas
+    check('rol').custom((rol) => rolValido(rol)),
+    //esto el lo mismo check('rol').custom( rolValido),
+
+    check('correo').custom((correo) => emailExistee(correo)),
+    validarCampos,
+], crearUsuario);
+
+router.put('/:idss', [
+    check('idss', 'No es un id valido').isInt({min: 1}),
+    check('idss').custom((idss) => existeId(idss)),
+    check('rol').custom((rol) => rolValido(rol)),
+    validarCampos
+], actualizarUsuario);
+
+router.delete('/:idss',[
+    check('idss', 'No es un id valido').isInt({min: 1}),
+    check('idss').custom((idss) => existeId(idss)),
+    validarCampos
+],
+eliminarUsuario);
+
+
+module.exports = router;
